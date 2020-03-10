0d562f233fb99f22ed0071b2e63ab3cbd8f8d4de
Jeff Lin <jeff.andrew.lin@gmail.com>
2020-03-10 02:25:06 -0400

    Maybe

---
+++ platforms/browser/www/cordova_plugins.js
@@ -0,0 +1,18 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-fullscreen/www/AndroidFullScreen.js",
+        "id": "cordova-plugin-fullscreen.AndroidFullScreen",
+        "pluginId": "cordova-plugin-fullscreen",
+        "clobbers": [
+            "AndroidFullScreen"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-fullscreen": "1.1.0"
+}
+// BOTTOM OF METADATA
+});
